429db3a550522959139696eda0f25afd302e274a
Scott Maclure <scott@maclure.com.au>
2013-06-11 08:21:58 +1000

    WIP setting up routes for http://docs.angularjs.org/tutorial/step_07

--- public/javascripts/angdo.js
+++ public/javascripts/angdo.js
@@ -3,8 +3,8 @@
  * Writing entire app in 1 JS file for now.
  * Linted via JSHint in IDE.
  *
- * TODO Load from a static json resource for starters.
  * TODO Later, load from local storage.
+ * TODO Separate file into individual files etc, gruntify.
  */
 
 // Define module with no deps.
@@ -13,6 +13,9 @@ var angDo = angular.module('angDo', []);
 // Debugging var in global scope.
 var angDoScope;
 
+/**
+ * Main todo list controller object.
+ */
 angDo.controller('TodoListController', function($scope, $http) {
 
     // Debugging
@@ -21,13 +24,30 @@ angDo.controller('TodoListController', function($scope, $http) {
 	// Load JSON data from server
 	$http.get('data/todoList.json')
 		 .then(function (res) {
-
 			//$scope.todoList = JSON.parse(res.data);
 			$scope.todoList = res.data;
-
-			// Set default order
-			//$scope.orderProp = 'dueDate';
-            $scope.orderProp = 'preferredOrder';
 		});
 
+    // Set default order
+    //$scope.orderProp = 'dueDate';
+    $scope.orderProp = 'preferredOrder';
+
 });
+
+/**
+ * Configure routes
+ */
+angDo.config(
+    ['$routeProvider', function ($routeProvider) {
+        $routeProvider
+            .when(
+                '/index',
+                { templateUrl: 'partials/index.html', controller: angDo.controller('TodoListController')}
+            )
+            .when(
+                '/todo/:todoId',
+                { templateUrl: 'partials/todo.html', controller: angDo.controller('TodoController')}
+            )
+            .otherwise({redirectTo: '/index'});
+    }]
+);
